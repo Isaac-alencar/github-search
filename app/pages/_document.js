5f0611b4744a851e144286c1fac9e96765217849
Isaac Alencar <alencarisaac3@gmail.com>
2021-05-15 10:59:27 -0300

    fix/removed metatag viewport and fix attribute name

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -10,12 +10,8 @@ class CustomDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           ></link>
-          <meta charset="UTF-8" />
+          <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
         </Head>
         <body>
           <Main />
