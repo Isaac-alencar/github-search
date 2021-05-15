6633e257f5d4acbe12dc70e992d51b98e0c9e94f
Isaac Alencar <alencarisaac3@gmail.com>
2021-05-14 23:08:10 -0300

    style/created homepage  - Created homepage to search for a user on github

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,3 +1,39 @@
+import { useState, useContext } from "react";
+
+import api from "../services/api";
+
+import { UserProfileContext } from "../contexts/userProfileContext";
+
+import styles from "../styles/pages/Home.module.scss";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  const [inputvalue, setInputValue] = useState("");
+  const { setUserProfileData } = useContext(UserProfileContext);
+
+  async function handleSearchUser(event) {
+    event.preventDefault();
+
+    const response = await api.get(`${inputvalue}`);
+    if (response.status === 200) {
+      setUserProfileData(response.data);
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <img src="/assets/logo-vertical.svg" alt="Github search" />
+      <div className={styles.inputContainer}>
+        <input
+          type="text"
+          placeholder="Enter user name"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputvalue}
+          required
+        />
+        <button type="button" onClick={(e) => handleSearchUser(e)}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
 }
