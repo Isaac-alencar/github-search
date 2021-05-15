a555bdf2dd05e2b5ddbad92dbc12c6e071df8d76
Isaac Alencar <alencarisaac3@gmail.com>
2021-05-14 21:21:16 -0300

    styles/remove uncessary files and folders + set global styles   - Remove default assets with create-next-app   - Craeted global styles with scss   - Created page _document.js to metatags, fonts

---
+++ app/pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Main, NextScript, Head } from "next/document";
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          ></link>
+          <meta charset="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
