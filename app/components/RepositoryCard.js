3747e3eca9d16c9a3b95d0b3fcb14fb5e7ea1802
Isaac Alencar <alencarisaac3@gmail.com>
2021-05-17 10:25:59 -0300

    refactor/Gettin informations dynamically from props  - Getting repositories informations from props in components  - Fixed margin-bottom between repositorie title & descriprion

--- app/components/RepositoryCard.js
+++ app/components/RepositoryCard.js
@@ -1,32 +1,45 @@
+import { useRouter } from "next/router";
+
 import { Star } from "@styled-icons/fluentui-system-filled";
 import { GitBranch, LinkExternal } from "@styled-icons/boxicons-regular";
-import { FileEarmarkCodeFill } from '@styled-icons/bootstrap/FileEarmarkCodeFill'
+import { FileEarmarkCodeFill } from "@styled-icons/bootstrap/FileEarmarkCodeFill";
 
 import styles from "../styles/components/RepositoryCard.module.scss";
 
-export function RepositoryCard() {
+export function RepositoryCard({
+  url,
+  stars,
+  name,
+  description,
+  languages,
+  forks,
+}) {
   return (
     <div className={styles.container}>
       <div>
-        <strong>moveit</strong>
-        <span>Projecte developed on NLW #4</span>
+        <strong>{name}</strong>
+        <span>{description}</span>
       </div>
 
       <div className={styles.aboutRepo}>
         <div>
-					<Star size={20} color="var(--purple)" />
-          <span>9815</span>
+          <Star size={20} color="var(--purple)" />
+          <span>{stars}</span>
         </div>
-				<div>
-					<GitBranch size={20} color="var(--purple)" />
-          <span>5</span>
+        <div>
+          <GitBranch size={20} color="var(--purple)" />
+          <span>{forks}</span>
         </div>
-				<div>
-					<FileEarmarkCodeFill size={20} color="var(--purple)" />
-          <span>Typescript</span>
+        <div>
+          <FileEarmarkCodeFill size={20} color="var(--purple)" />
+          <span>{languages}</span>
         </div>
-				<div>
-					<LinkExternal size={20} color="var(--purple)" />
+        <div>
+          <LinkExternal
+            size={20}
+            color="var(--purple)"
+            onClick={() => window.open(`${url}`, "_blank")}
+          />
         </div>
       </div>
     </div>
